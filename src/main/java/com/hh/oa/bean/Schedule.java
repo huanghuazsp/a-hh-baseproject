3a701830395dac8dedf88192eb2e477cd08b9b1f
huanghuazsp <huanghuazsp@183e5d52-12aa-4c75-83e6-4e6be8f07bd4>
2016-12-01 13:28:04 +0000

    hibernate表注释扩展

--- src/main/java/com/hh/oa/bean/Schedule.java
+++ src/main/java/com/hh/oa/bean/Schedule.java
@@ -10,8 +10,10 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
+import com.hh.hibernate.dao.inf.Comment;
 import com.hh.hibernate.util.base.BaseTwoEntity;
 
+@Comment("日程安排")
 @Entity
 @Table(name = "SYS_SCHEDULE")
 public class Schedule extends BaseTwoEntity {
@@ -27,13 +29,15 @@ public class Schedule extends BaseTwoEntity {
 	
 	private String userId;
 	
-	
+	@Comment("是否全天")
 	public boolean isAllDay() {
 		return allDay;
 	}
 	public void setAllDay(boolean allDay) {
 		this.allDay = allDay;
 	}
+	
+	@Comment("用户id")
 	@Column(name = "USER_ID",length=36)
 	public String getUserId() {
 		return userId;
@@ -42,7 +46,7 @@ public class Schedule extends BaseTwoEntity {
 		this.userId = userId;
 	}
 	
-	
+	@Comment("日程内容")
 	@Lob
 	@Column(name = "CONTENT_")
 	public String getContent() {
@@ -60,6 +64,8 @@ public class Schedule extends BaseTwoEntity {
 	public void setParticipants(String participants) {
 		this.participants = participants;
 	}
+	
+	@Comment("日程开始时间")
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "START_", length = 7)
 	public Date getStart() {
@@ -69,6 +75,7 @@ public class Schedule extends BaseTwoEntity {
 		this.start = start;
 	}
 	
+	@Comment("日程结束时间")
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "END_", length = 7)
 	public Date getEnd() {
@@ -77,12 +84,15 @@ public class Schedule extends BaseTwoEntity {
 	public void setEnd(Date end) {
 		this.end = end;
 	}
+	
+	@Comment("是否完成")
 	public int getIsOk() {
 		return isOk;
 	}
 	public void setIsOk(int isOk) {
 		this.isOk = isOk;
 	}
+	@Comment("日程优先级")
 	@Column(name = "LEVEL_",length=2)
 	public String getLevel() {
 		return level;
